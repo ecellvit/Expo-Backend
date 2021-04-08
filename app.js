c9e7e1a6c0ba0318b4a2fcc51f840dd6105b76a4
Rehaan Mazid <rehaanmazid@gmail.com>
2021-04-08 21:11:54 +0530

    feat: authentication done

---
+++ app.js
@@ -0,0 +1,42 @@
+require('dotenv').config()
+const express = require('express')
+const users = require('./routes/users')
+const mongoose = require('mongoose')
+const session = require('express-session')
+const passport = require('passport')
+
+const app = express()
+
+// passport config
+require('./config/passport')(passport)
+
+const PORT = process.env.PORT
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+// Express Session
+app.use(session({
+  secret: process.env.SECRET,
+  resave: true,
+  saveUninitialized: true
+}))
+
+// Passport
+app.use(passport.initialize())
+app.use(passport.session())
+
+// Connect to Mongo
+mongoose.connect(process.env.MONGODB_DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('MongoDb Connected ......')
+  })
+  .catch((err) => {
+    console.log('Error:', err)
+  })
+
+app.use('/users', users)
+
+app.listen(PORT, () => {
+  console.log('Server Started on port', PORT)
+})
---
+++ routes/users.js
@@ -0,0 +1,103 @@
+const router = require('express').Router()
+const bcrypt = require('bcryptjs')
+const passport = require('passport')
+const { ensureAuthenthicated } = require('../config/auth')
+
+const User = require('../models/User')
+
+router.post('/login', (req, res, next) => {
+  passport.authenticate('local', {
+    successRedirect: '/users/success',
+    failureRedirect: '/users/failure'
+  })(req, res, next)
+})
+
+router.get('/success', (req, res) => {
+  return res.status(200).json({
+    message: 'logged In'
+  })
+})
+
+router.get('/failure', (req, res) => {
+  return res.status(400).json({
+    message: 'log in denied'
+  })
+})
+
+router.get('/dashboard', ensureAuthenthicated, (req, res) => {
+  return res.status(400).json({
+    message: req.user.name + ' Logged In'
+  })
+})
+
+router.get('/logout', (req, res) => {
+  req.logout()
+  return res.status(200).json({
+    message: 'logged out'
+  })
+})
+
+router.post('/register', (req, res) => {
+  if (!req.body.name || !req.body.resumeLink || !req.body.email || !req.body.password || !req.body.phoneNo) {
+    return res.status(400).json({
+      erroMessage: 'missing required parameters. refer documentation'
+    })
+  }
+
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (user) {
+        return res.status(400).json({
+          erroMessage: 'user already exists. please login'
+        })
+      } else {
+        const name = req.body.name
+        const email = req.body.email
+        const phoneNo = req.body.phoneNo
+        const password = req.body.password
+        const resumeLink = req.body.resumeLink
+
+        const newUser = new User({
+          name,
+          password,
+          email,
+          phoneNo,
+          resumeLink
+        })
+
+        // hash
+        bcrypt.genSalt(10, (err, salt) => {
+          if (err) {
+            return res.status(400).json({
+              erroMessage: err
+            })
+          }
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) {
+              return res.status(400).json({
+                erroMessage: err
+              })
+            }
+
+            newUser.password = hash
+            newUser.save()
+              .then((user) => {
+                return res.status(200).json({
+                  message: 'success'
+                })
+              })
+              .catch((err) => {
+                return res.status(400).json({
+                  erroMessage: err
+                })
+              })
+          })
+        })
+      }
+    })
+    .catch((err) => {
+      console.log('Error:', err)
+    })
+})
+
+module.exports = router
