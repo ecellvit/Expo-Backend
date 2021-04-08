94e554d7cc800ad5dde5104f50be38a1e0ef015c
Rehaan Mazid <rehaanmazid@gmail.com>
2021-04-08 23:42:36 +0530

    feat: company done csv left

--- routes/company.js
+++ routes/company.js
@@ -58,12 +58,43 @@ router.post('/add', ensureAuthenthicated, (req,res)=>{
       })
 })
 
+router.get('/getAll',ensureAuthenthicated,(req,res)=>{
+    Company.find()
+        .then((infos)=>{
+            res.status(200).json(infos)
+        })
+})
+
+router.post('/getData',ensureAuthenthicated,(req,res)=>{
+    if (!req.body.name) {
+        return res.status(400).json({
+          erroMessage: 'missing required parameters. refer documentation'
+        })
+    }
+    
+    Company.findOne({ name: req.body.name })
+        .then((company)=>{
+            if (!company) {
+                return res.status(400).json({
+                    erroMessage: 'company does not exist'
+                })
+            }
+            else
+            {
+                return res.status(200).json(company)
+            }
+        })
+        .catch((err) => {
+            console.log('Error:', err)
+        })
+})
+
 router.post('/addSlot', ensureAuthenthicated, (req,res)=>{
     if (!req.body.name || !req.body.startTime || !req.body.endTime) {
         return res.status(400).json({
           erroMessage: 'missing required parameters. refer documentation'
         })
-      }
+    }
     
     Company.findOne({ name: req.body.name })
       .then((company)=>{
@@ -103,5 +134,117 @@ router.post('/addSlot', ensureAuthenthicated, (req,res)=>{
         console.log('Error:', err)
       })
 })
+
+router.patch('/updateSlot', ensureAuthenthicated, (req,res)=>{
+    if (!req.body.name || !req.body.id || !req.body.startTime || !req.body.endTime ) {
+        return res.status(400).json({
+          erroMessage: 'missing required parameters. refer documentation'
+        })
+      }
+    
+    Company.findOne({ name: req.body.name })
+      .then((company)=>{
+        if (!company) {
+            return res.status(400).json({
+              erroMessage: 'company does not exist'
+            })
+        }
+        else{
+
+            const slots = company.slots
+            
+            for (let i = 0; i < slots.length; i++) {
+                if (slots[i]._id.equals(req.body.id)) {
+                    slots[i].startTime = req.body.startTime
+                    slots[i].endTime = req.body.endTime
+                    if(req.body.available)
+                    {
+                        slots[i].available = req.body.available
+                    }
+                    if(req.body.total)
+                    {
+                        slots[i].total = req.body.total
+                    }
+                }
+            }
+
+            Company.updateOne({ name: req.body.name },
+                { $set: { slots: slots} })
+                .then((update) => {
+                  res.status(200).json({
+                    message: 'slot updated, details updated in db'
+                  })
+                })
+                .catch((err) => {
+                  console.log('Error:', err)
+                })
+
+        }
+      })
+      .catch((err) => {
+        console.log('Error:', err)
+      })
+})
+
+router.delete('/deleteSlot', ensureAuthenthicated, (req,res)=>{
+    if (!req.body.name || !req.body.id ) {
+        return res.status(400).json({
+          erroMessage: 'missing required parameters. refer documentation'
+        })
+      }
+    
+    Company.findOne({ name: req.body.name })
+      .then((company)=>{
+        if (!company) {
+            return res.status(400).json({
+              erroMessage: 'company does not exist'
+            })
+        }
+        else{
+
+            const slots = company.slots
+            
+            for (let i = 0; i < slots.length; i++) {
+                if (slots[i]._id.equals(req.body.id)) {
+                    slots.splice(i, 1)
+                }
+            }
+
+            Company.updateOne({ name: req.body.name },
+                { $set: { slots: slots} })
+                .then((update) => {
+                  res.status(200).json({
+                    message: 'slot deleted, details updated in db'
+                  })
+                })
+                .catch((err) => {
+                  console.log('Error:', err)
+                })
+
+        }
+      })
+      .catch((err) => {
+        console.log('Error:', err)
+      })
+})
+
+router.delete('/deleteCompany', ensureAuthenthicated, (req,res)=>{
+    if (!req.body.name) {
+        return res.status(400).json({
+          message: 'missing required parameters. refer documentation'
+        })
+      }
+    
+    Company.deleteOne({name: req.body.name})
+      .then(()=>{
+        return res.status(200).json({
+            erroMessage: 'Deleted'
+          })
+      })
+      .catch((err) => {
+        console.log('Error:', err)
+      })
+})
+
 
 module.exports = router
